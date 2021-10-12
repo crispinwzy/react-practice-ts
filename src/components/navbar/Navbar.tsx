b8da07143b34c7a1b13b3c90fcd0455bc02135b4
Criss <wuzheyun@gmail.com>
2021-10-12 23:44:57 +0200

    Update style

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,21 +1,21 @@
 import { Link } from 'react-router-dom';
-
 import { useAppSelector, useAppDispatch } from 'app/hooks';
 import { AuthState, selectAuth, logout} from 'features/auth/authSlice';
+import './Navbar.scss';
 
 export function Navbar() {
   const dispatch = useAppDispatch();
   const authState: AuthState = useAppSelector(selectAuth);
 
   return (
-    <nav>
-      <ul>
+    <nav className="navbar">
+      <ul className="left">
         <li><Link to='/'>Home</Link></li>
         <li><Link to='/register'>Register</Link></li>
         <li><Link to='/profile'>Profile</Link></li>
       </ul>
 
-      <div>
+      <div className="right">
         Hello, {authState.isLoggedin ? authState.name : 'Guest'}!
         {authState.isLoggedin && <button onClick={() => dispatch(logout())}>Logout</button>}
       </div>
