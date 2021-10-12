bf45a71d80364aff7d61b95acd1fb14d3ce32681
Criss <wuzheyun@gmail.com>
2021-10-12 17:20:07 +0200

    Remove useless route

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -12,7 +12,6 @@ export function Navbar() {
         <li><Link to='/'>Home</Link></li>
         <li><Link to='/register'>Register</Link></li>
         <li><Link to='/profile'>Profile</Link></li>
-        <li><Link to='/counter'>Counter</Link></li>
       </ul>
 
       <div>
