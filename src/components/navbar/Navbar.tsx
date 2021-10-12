8990b9b4efffed8beb12e424b249380072823705
Criss <wuzheyun@gmail.com>
2021-10-13 01:12:29 +0200

    Optimize navbar layout

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -17,15 +17,21 @@ export const Navbar: FunctionComponent = () => {
 
   return (
     <nav className="navbar">
-      <ul className="left">
-        <li><Link to='/'>Home</Link></li>
-        <li><Link to='/register'>Register</Link></li>
-        <li><Link to='/profile'>Profile</Link></li>
-      </ul>
+      <div className="left">
+        <span className="brand-name">React Practice</span>
+        <ul>
+          <li><Link to='/'>Home</Link></li>
+          {/* <li><Link to='/register'>Register</Link></li> */}
+          <li><Link to='/profile'>Profile</Link></li>
+        </ul>
+      </div>
 
       <div className="right">
         Hello, {authState.isLoggedin ? authState.name : 'Guest'}!
-        {authState.isLoggedin && <button onClick={handleLogout}>Logout</button>}
+        
+        <span className="actions">
+          {authState.isLoggedin ? <button onClick={handleLogout}>Logout</button> : <Link to='/register'>Register</Link>}
+        </span>
       </div>
     </nav>
   );
