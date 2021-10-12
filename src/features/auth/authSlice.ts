643000610cf4950eb36635084ef95ed860aacabb
Criss <wuzheyun@gmail.com>
2021-10-12 17:03:46 +0200

    Add store(Redux) slice: authSlice

---
+++ src/features/auth/authSlice.ts
@@ -0,0 +1,56 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { RootState } from '../../app/store';
+
+// Define auth-related types
+export interface AuthState {
+  isLoggedin: boolean,
+  name: string,
+  email: string
+}
+
+export interface RegisterPayload {
+  name: string,
+  email: string,
+  password: string
+}
+
+// Define initial auth state
+const initialState: AuthState = {
+  isLoggedin: false,
+  name: '',
+  email: '',
+};
+
+// let preloadedState;
+// const persistedTodosString = localStorage.getItem('auth');
+
+// if (persistedTodosString) {
+//   preloadedState = {
+//     auth: JSON.parse(persistedTodosString)
+//   }
+// }
+
+export const authSlice = createSlice({
+  name: 'auth',
+  initialState,
+  reducers: {
+    register: (state, action: PayloadAction<RegisterPayload>) => {
+      const {name, email} = action.payload;
+
+      state.isLoggedin = true;
+      state.name = name.trim();
+      state.email = email.trim();
+    },
+    logout: (state) => {
+      state.isLoggedin = false;
+      state.name = '';
+      state.email = '';
+    }
+  },
+});
+
+export const { register } = authSlice.actions;
+
+export const selectAuth = (state: RootState) => state.auth;
+
+export default authSlice.reducer;
