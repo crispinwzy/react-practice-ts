b3b188d814fb5d8b0c061ce19934030cc5bada24
Criss <wuzheyun@gmail.com>
2021-10-18 04:18:47 +0200

    Use weather in profile page

--- src/pages/profile/Profile.tsx
+++ src/pages/profile/Profile.tsx
@@ -1,16 +1,22 @@
+import './Profile.scss';
 import { useAppSelector } from 'app/hooks';
 import { AuthState, selectAuth } from 'app/store/slices/authSlice';
-import './Profile.scss';
+import { Weather } from 'components/weather/Weather';
 
 export function Profile() {
   const auth: AuthState = useAppSelector(selectAuth);
 
   return (
-    <div className="profile-page">
-      <h3>Profile</h3>
-      <p>isLoggedin: {auth.isLoggedin ? 'Yes' : 'No'}</p>
-      <p>Name: {auth.name}</p>
-      <p>Email: {auth.email}</p>
+    <div>
+      <div className="profile-page">
+        <h3>Profile</h3>
+        <p>isLoggedin: {auth.isLoggedin ? 'Yes' : 'No'}</p>
+        <p>Name: {auth.name}</p>
+        <p>Email: {auth.email}</p>
+      </div>
+
+      {/* // Weather component */}
+      <Weather />
     </div>
   );
 }
