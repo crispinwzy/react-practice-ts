48f2d54b5bd8d9115a42f907eabde1b4933bc388
Criss <wuzheyun@gmail.com>
2021-10-18 04:15:32 +0200

    Update home page

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -4,10 +4,7 @@ export function Home() {
   return (
     <div className="home-page">
       <h3>Home Page</h3>
-      <h4>Description</h4>
-      <p>This is React practice project created by Zheyun Wu</p>
-      <h4>Stockholm Weather</h4>
-      <p>[TO BE ADDED......]</p>
+      <p>This is a React practice project by Zheyun Wu</p>
     </div>
   );
 }
